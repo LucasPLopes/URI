5f95c427bc130a4eb8ace6ac0fcf964d064960c5
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2018-10-25 10:57:08 -0300

    Create 2612_v1.sql

---
+++ SQL/2612_v1.sql
@@ -0,0 +1,8 @@
+ --2612 Os Atores Silva
+ 
+ select m.id as id, m.name as name  from movies as m
+ inner join genres as g on m.id_genres = g.id where
+ g.description='Action' and m.id in 
+ (select distinct ma.id_movies from movies_actors as ma inner join 
+ actors as a on a.id= ma.id_actors where a.name like '%silva%');
+	
