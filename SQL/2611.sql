e23cf22b575ae6dcc8f5d5d0857a408c87c16ea6
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-08-24 11:50:45 -0300

    Refactor
    Using alias for table's name.

--- SQL/2611.sql
+++ SQL/2611.sql
@@ -1,2 +1,2 @@
 --2611 Filmes de Ação
- select movies.id as id, movies.name as name from movies,genres where movies.id_genres=genres.id and description='Action';
+ select m.id as id, m.name as name from movies as m,genres as g where m.id_genres=g.id and description='Action';
