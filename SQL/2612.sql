1b81bace7a64394cde940074c81980fb2048ea5d
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-08-24 12:07:00 -0300

    Refactor
    Removed inner join to use only join and also removed comparation by equals for use of like comparator.

--- SQL/2612.sql
+++ SQL/2612.sql
@@ -1,7 +1,5 @@
-
  select m.id as id, m.name as name  from movies as m
- inner join genres as g on m.id_genres = g.id where
+ join genres as g on m.id_genres = g.id where
  g.description='Action' and m.id in 
- (select distinct ma.id_movies from movies_actors as ma inner join 
- actors as a on a.id= ma.id_actors where a.name='Marcelo Silva' or a.name='Miguel Silva' );
-	
+ (select distinct ma.id_movies from movies_actors as ma join 
+ actors as a on a.id= ma.id_actors where a.name like 'Marcelo S%' or a.name like 'Miguel S%' );
