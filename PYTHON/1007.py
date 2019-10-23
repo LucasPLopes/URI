d4dd6550d64bc33a9ccd545980352300f1b7dc59
Lucas <l.lopes.fga@gmail.com>
2019-10-23 01:11:27 -0300

    1007

---
+++ PYTHON/1007.py
@@ -0,0 +1,15 @@
+
+# DIFERENCA = (A * B - C * D)
+def diferenca(a,b,c,d):
+    return a*b -c *d
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+
+dif = diferenca(a,b,c,d)
+
+print('DIFERENCA = {}'.format(dif))
