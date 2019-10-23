4348a3a9c11461c4054f21a5cab04074c1eb59fb
Lucas <l.lopes.fga@gmail.com>
2019-10-23 01:12:09 -0300

    1011

--- PYTHON/1011.py
+++ PYTHON/1011.py
@@ -1,7 +1,16 @@
-def pi():
+"""
+ Como no Python não há constantes
+ criamos uma função que retorna o valor constante
+"""
+def pi():  
     return 3.14159
+
+# Potência em python  a ^ b => a **b    ex.: 2 ** 3 == 8
+def area(v):
+    return 4/3.0 *pi() * v ** 3   
 
 v = float(input())
 
-volume = 4/3.0 *pi() * v * v * v
+volume = area(v)
+
 print ("VOLUME = %.3f"% volume)
