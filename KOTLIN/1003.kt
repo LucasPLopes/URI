4961d017194de6c6d04e5bf1ea7c93a5bd6390ff
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-03 13:53:28 -0200

    1003 kotlin

---
+++ KOTLIN/1003.kt
@@ -0,0 +1,19 @@
+import java.util.Scanner
+
+fun main(args:Array<String>){
+    val sc = Scanner(System.`in`)
+    
+    var a :String = sc.next()
+    var b :String = sc.next()
+    var res : Int = 0
+
+    res = soma(a.toInt(),b.toInt())
+    println(saida(res))
+}
+
+fun soma(a: Int, b:Int ):Int = a + b
+
+fun saida( vlr:Int):String{
+    var resposta:String = "SOMA = ${vlr}"
+    return resposta  
+}
