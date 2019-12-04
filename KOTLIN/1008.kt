5792d345b71e80c781240a2a19183d31c9ae1baf
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-04 10:45:02 -0200

    1008 kotlin

---
+++ KOTLIN/1008.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main(args:Array<String>){
+    val sc = Scanner(System.`in`)
+    var n : Int = sc.next().toInt()
+    var h : Int = sc.next().toInt()
+    var p : Double = sc.next().toDouble()
+    
+    println(number(n))
+    println(salary(h, p))
+
+}
+
+fun number(n:Int):String  ="NUMBER = $n"
+fun salary(h:Int, p:Double):String = "SALARY = U$ %.2f".format(h * p)
