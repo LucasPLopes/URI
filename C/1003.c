60bc4f3374349c0268f30838e1e47db301b0ad34
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-17 13:05:49 -0200

    1003 C refatora

--- C/1003.c
+++ C/1003.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
+
+void ler(int *a, int *b)
+{
+    scanf("%i%i", &(*a), &(*b));
+}
+int soma(int *a, int *b)
+{
+    return *a + *b;
+}
+void imprime(int c)
+{
+    char * msg = "SOMA = %i\n";
+    printf(msg, c);
+}
 int main()
 {
 
     int a, b, c = 0;
-    scanf("%i%i", &a, &b);
-    c = a + b;
-    printf("SOMA = %i\n", c);
+
+    ler(&a, &b);
+
+    c = soma(&a,&b);
+    imprime(c);
     return 0;
 }
