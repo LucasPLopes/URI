68e54f858eefd32e52f12a7957a1281fcc0a8386
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-17 13:26:14 -0200

    1005 C refatora

--- C/1005.c
+++ C/1005.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 
+const double N1 = 3.5;
+const double N2 = 7.5;
+
+void ler(double *n1, double *n2)
+{
+    scanf("%lf%lf", &(*n1), &(*n2));
+}
+void calc(double *n1, double *n2)
+{
+    *n1 *= N1; // *n1 = (*n1) * N1;
+    *n2 *= N2;
+}
+double mediaAlt(double *n1, double *n2)
+{
+    return (*n1 + *n2) / (N1 + N2);
+}
+
+void imprime(double media)
+{
+    char *msg = "MEDIA = %.5lf\n";
+    printf(msg, media);
+}
+
 int main()
 {
 
     double n1, n2, media;
-    scanf("%lf%lf", &n1, &n2);
-    n1 = n1 * 3.5;
-    n2 = n2 * 7.5;
-    media = (n1 + n2) / 11.0;
 
-    printf("MEDIA = %.5lf\n", media);
+    ler(&n1, &n2);
+    calc(&n1, &n2);
+    media = mediaAlt(&n1, &n2);
+    imprime(media);
 
     return 0;
 }
