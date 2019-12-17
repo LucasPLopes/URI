1ce70c01c349b888c1586f69af16a7cd91fb9c2d
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-17 13:15:41 -0200

    1004 C refatora

--- C/1004.c
+++ C/1004.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
+void ler(int *a, int *b)
+{
+    scanf("%i%i", &(*a), &(*b));
+}
+int mult(int *a, int *b)
+{
+    return *a * *b;
+}
+void imprime(int *c)
+{
+    char *msg = "PROD = %i\n";
+    printf(msg, *c);
+}
+
 int main()
 {
 
     int a, b, c = 0;
-    scanf("%i%i", &a, &b);
-    c = a * b;
-    printf("PROD = %i\n", c);
+    ler(&a, &b);
+    c = mult(&a, &b);
+    imprime(&c);
     return 0;
 }
